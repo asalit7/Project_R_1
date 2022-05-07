84968ab1773d10fce5def29d6aa6911690ff80db
AlecS <alec.salit@gmail.com>
2022-05-07 14:35:14 -0400

    Created Death City Bar Grph

--- OD_CT_Project.R
+++ OD_CT_Project.R
@@ -8,9 +8,11 @@ library(ggplot2)
 df <- read.csv('C:\\Users\\alecs\\Project_R\\Drug_Deaths.csv')
 summary(df)
 
+city <- df %>% filter(!ResidenceCity == '') %>% group_by(ResidenceCity) %>%
+  summarise(total = n()) %>% arrange(desc(total))
+ggplot(head(city, 15), aes(x = reorder(ResidenceCity, total), y = total)) + 
+  geom_bar(stat='identity',fill='orange') + coord_flip() + labs(x = "Cities", y = "Total Deaths") + ggtitle('Death Count Top 15 Cities')
+
 df %>% select(ResidenceState, ResidenceCity, ResidenceCounty, AnyOpioid)%>%
   summarise(total = sum(AnyOpioid))%>%
   arrange(ResidenceCity)
-
-city <- df %>% group_by(ResidenceCity) %>%
-  summarise(total = n()) %>% arrange(desc(total))
