5b747764c47723d293c92a7f39de9e4b3d179036
AlecS <alec.salit@gmail.com>
2022-05-07 15:05:47 -0400

    Created area graph for ages

--- OD_CT_Project.R
+++ OD_CT_Project.R
@@ -11,8 +11,7 @@ summary(df)
 city <- df %>% filter(!ResidenceCity == '') %>% group_by(ResidenceCity) %>%
   summarise(total = n()) %>% arrange(desc(total))
 ggplot(head(city, 15), aes(x = reorder(ResidenceCity, total), y = total)) + 
-  geom_bar(stat='identity',fill='orange') + coord_flip() + labs(x = "Cities", y = "Total Deaths") + ggtitle('Death Count Top 15 Cities')
+  geom_bar(stat='identity',fill='orange') + coord_flip() + labs(x = "Cities", y = "Total Deaths") + ggtitle('Death Count By Top 15 Cities')
 
-df %>% select(ResidenceState, ResidenceCity, ResidenceCounty, AnyOpioid)%>%
-  summarise(total = sum(AnyOpioid))%>%
-  arrange(ResidenceCity)
+age <- df  %>% group_by(Age) %>% summarise(total = n())
+ggplot(na.omit(age), aes(x = Age, y=total)) + geom_area(fill='red') + labs(y= 'Total Deaths') + ggtitle ('Death Count By Age')
