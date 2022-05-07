aa6adece3ff9c8adc818d6841be54ea1ca0c0e6b
AlecS <alec.salit@gmail.com>
2022-05-07 11:09:48 -0400

    Creating table for highest city deaths

--- OD_CT_Project.R
+++ OD_CT_Project.R
@@ -1,3 +1,16 @@
 #Drug Overdose Stats
+
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
+
 df <- read.csv('C:\\Users\\alecs\\Project_R\\Drug_Deaths.csv')
 summary(df)
+
+df %>% select(ResidenceState, ResidenceCity, ResidenceCounty, AnyOpioid)%>%
+  summarise(total = sum(AnyOpioid))%>%
+  arrange(ResidenceCity)
+
+city <- df %>% group_by(ResidenceCity) %>%
+  summarise(total = n()) %>% arrange(desc(total))
