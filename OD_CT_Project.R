0267101d522050ee8da7081e22c5b8b17cb6e012
AlecS <alec.salit@gmail.com>
2022-05-08 00:20:44 -0400

    Adding Gender bar graph

--- OD_CT_Project.R
+++ OD_CT_Project.R
@@ -10,8 +10,12 @@ summary(df)
 
 city <- df %>% filter(!ResidenceCity == '') %>% group_by(ResidenceCity) %>%
   summarise(total = n()) %>% arrange(desc(total))
+
 ggplot(head(city, 15), aes(x = reorder(ResidenceCity, total), y = total)) + 
   geom_bar(stat='identity',fill='orange') + coord_flip() + labs(x = "Cities", y = "Total Deaths") + ggtitle('Death Count By Top 15 Cities')
 
 age <- df  %>% group_by(Age) %>% summarise(total = n())
-ggplot(na.omit(age), aes(x = Age, y=total)) + geom_area(fill='red') + labs(y= 'Total Deaths') + ggtitle ('Death Count By Age')
+ggplot(na.omit(age), aes(x = Age, y=total)) + geom_count(fill='red') + labs(y= 'Total Deaths') + ggtitle('Death Count By Age')
+
+gender <- df %>% filter(Sex == "Male" | Sex == "Female") %>% group_by(Sex) %>% summarise(total = n())
+ggplot(gender, aes(x = reorder(Sex, total), y=total)) + geom_bar(stat="identity",fill ="blue")
